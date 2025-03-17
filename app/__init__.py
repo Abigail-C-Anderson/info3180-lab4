046e315690f3aef65fee1d27e23457c192a287b3
Abigail Anderson <abigailchrisann2@gmail.com>
2025-03-16 22:20:11 -0500

    Exercise 2 Completed

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash
 # import flask migrate here
 
 app = Flask(__name__)
